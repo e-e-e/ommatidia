f6aa237691219feeee8b1fbc9b196672bb9caa47
e-e-e <e-e-e@users.noreply.github.com>
2017-01-07 19:30:22 +1100

    add const for facets

---
+++ src/consts/index.js
@@ -0,0 +1,14 @@
+
+export const PERSONALITY = 'personality';
+export const MATTER = 'matter';
+export const ENERGY = 'energy';
+export const SPACE = 'space';
+export const TIME = 'time';
+
+export const FACETS = [
+  PERSONALITY,
+  MATTER,
+  ENERGY,
+  SPACE,
+  TIME,
+];
